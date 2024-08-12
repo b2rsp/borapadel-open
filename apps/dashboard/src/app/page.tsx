cf7cb4dc2129db3441f347b9fe13d23a1c328abf
Vitor Gonçalves <vitorgoncalves57@gmail.com>
2024-08-12 09:27:07 +0100

    Added the prisma client to project

--- apps/dashboard/src/app/page.tsx
+++ apps/dashboard/src/app/page.tsx
@@ -1,6 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { PrismaClient } from "@borapadel/prisma";
 
-export default function Home() {
+const prisma = new PrismaClient();
+
+export default async function Home() {
+  const usersWithPosts = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.dir(usersWithPosts);
   return (
     <div>
       <Button>Click me</Button>
